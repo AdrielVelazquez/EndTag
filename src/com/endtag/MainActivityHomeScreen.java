28c8e748a0f70a62935b9ae88de6d3483cb46712
Adriel Velazquez <AdrielVelazquez@gmail.com>
2013-03-19 06:12:18 -0400

    forgot to implement listener on homescreen

--- src/com/endtag/MainActivityHomeScreen.java
+++ src/com/endtag/MainActivityHomeScreen.java
@@ -7,10 +7,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivityHomeScreen extends Activity {
+public class MainActivityHomeScreen extends Activity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +40,26 @@ public class MainActivityHomeScreen extends Activity {
         
         
         
+    }
+    
+    @Override
+    public void onClick (View view) {
+        // TODO Auto-generated method stub
+    	
+
+    	EditText name = (EditText)findViewById(R.id.Username);
+	    String strname = name.getText().toString();
+	    
+
+
+          Log.i("clicks","You Clicked Login");
+          	Intent i=new Intent(
+             MainActivityHomeScreen.this,
+             MainActivityLessonsList.class);
+          	 i.putExtra("Username", strname);
+      startActivity(i);
+
+
     }
     
 }
